2d55ea854a1ba0be8dbd513aaf2e8433211ac415
Allan FORSON <allan.forson@louis-armand.paris>
2017-12-14 16:48:55 +0100

    Ajout de discussions dans le front-end

---
+++ components/com_arvie/models/discussion.php
@@ -0,0 +1,41 @@
+<?php
+defined('_JEXEC') or die('Restricted access');
+ 
+class ArvieModelDiscussion extends JModelItem
+{
+	protected $_item = null;
+	protected $_context = 'com_arvie.discussion';
+
+	protected function populateState()
+	{
+		$app = JFactory::getApplication('site');
+		
+		// Charge et mémorise l'état (state) de l'id depuis le contexte
+		$pk = $app->input->getInt('id');
+		$this->setState($this->_context.'.id', $pk);
+		// $this->setState('groupe.id', $pk);
+	}
+
+	public function getItem($pk = null)
+	{
+		// Initialise l'id
+		$pk = (!empty($pk)) ? $pk : (int) $this->getState($this->_context.'.id');
+
+		// Si pas de données chargées pour cet id
+		if (!isset($this->_item[$pk])) {
+			$db = $this->getDbo();
+			$query = $db->getQuery(true);
+			$query->select('d.id, d.nom');
+			$query->from('#__arvie_discussions d');
+
+			// joint la table parent pour recuperer nom du groupe_parent
+			// $query->select('gp.nom AS groupe_parent_nom')->join('LEFT', '#__arvie_groupes AS gp ON gp.id=g.groupe_parent');
+		
+			$query->where('d.id = ' . (int) $pk);
+			$db->setQuery($query);
+			$data = $db->loadObject();
+			$this->_item[$pk] = $data;
+		}
+  		return $this->_item[$pk];
+	}
+}
